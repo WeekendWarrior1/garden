c56e90c9886c037cffff4fef729e9bac792af5b4
dwimmer-tm <dwimmer.tm@pm.me>
2021-10-06 13:54:22 +1100

    added basic square function

--- 0/dwimmer-thue_morse_sequence.py
+++ 0/dwimmer-thue_morse_sequence.py
@@ -1,28 +1,31 @@
 # dwimmer 2021
+import sys
+from PIL import Image, ImageDraw
 
 class Sequence:
 	''' A simple program to explore (some of) the interesting properties of the
 		Thue-Morse sequence. 
 	'''
 	def __init__(self, time, base):
-		self.time = time
+		self.time = time 
 		self.base = base
-		self.sequence = self.generate()
+		self.sequence = self.generate([i % self.base for i in range(self.base)], self.time)
+		self.sequence_square = self.generate(self.sequence, (self.time + 1))
 		self.number_set = [i for i in range(1,len(self.sequence)+1)]
 
-	def generate(self, bus=[]):
+	def generate(self, sequence, length):
 		""" Generate a Thue-Morse sequence with user specified base and time 
 			values. Utilises modulo to allow base numbers other than 2.
 		"""
-		sequence = [i % self.base for i in range(self.base)]
-		for loop in range(self.time):
+		bus = []
+		for loop in range(length):
 			for e in sequence:
 				for i in range(self.base):
 					bus.append((i+e) % self.base)
 			sequence = []
 			sequence.extend(bus)
 			bus = []
-		return sequence		
+		return sequence			
 
 	def sum_set(self, key, bus=[]):
 		""" Iterates over the generated Thue-Morse and linear number sequences,
@@ -43,12 +46,47 @@ class Sequence:
 
 
 
-x = Sequence(3, 2)
-print(x.sequence)
+		
+
+def divide(a, b):
+	try:
+		return a / b
+	except ZeroDivisionError:
+		return 0
+
+
+
+# please ignore the below ugliness, it's just scratchpad stuff atm
+
+
+
+
+
+	
+
+x = Sequence(5, 3)
+#print(x.sequence)
 for key in range(x.base):
 	x.sum_set(key)
+print(x.sequence)
+print(x.sequence_square)
+dx = 0
+dy = 0
+dimension = len(x.sequence)
+print(dimension)
+im = Image.new("RGB",(dimension, dimension))
+draw = ImageDraw.Draw(im)
+for e in x.sequence_square:
+	draw.point((dx,dy), 'hsl({},100%,50%)'.format(divide(360,e)))
+	dx += 1
+	if dx == (dimension):
+		dy += 1
+		dx = 0
+	#dx += 1
 
 
+
+im.save("test.png", "PNG")
 
 
 
