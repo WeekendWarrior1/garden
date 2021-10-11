b1618d7962c02ec16472f8d39c538ab1016cd1eb
dwimmer-tm <dwimmer.tm@pm.me>
2021-10-11 16:49:13 +1100

    simplified Sequence and Fractal into single class, streamlined methods, general housekeeping

--- 0/dwimmer-thue_morse_sequence.py
+++ 0/dwimmer-thue_morse_sequence.py
@@ -1,52 +1,91 @@
 # dwimmer 2021
-import sys
-from PIL import Image, ImageDraw
+import sys, operator
+from PIL import Image, ImageDraw, ImageOps
+
 
 class Sequence:
-	''' A simple program to explore (some of) the interesting properties of the
+	'''
+		A simple program to explore (some of) the interesting properties of the
 		Thue-Morse sequence. 
 	'''
-	def __init__(self, time, base):
-		self.time = time 
+	def __init__(self, base, time, index, operator="+"):
+		""" 
+		
+		"""
 		self.base = base
-		self.sequence = self.generate([i % self.base for i in range(self.base)], self.time)
-		self.sequence_square = self.generate(self.sequence, (self.time + 1))
-		self.number_set = [i for i in range(1,len(self.sequence)+1)]
+		self.time = time 
+		self.index = index
+		self.name = f'n{self.base}t{self.time}i{self.index}'
+		self.sequence = self.generate(
+			[(ops[operator](i, index)) % self.base for i in range(self.base)], 
+			(self.time - 1)
+			)
+		self.sequence_square = self.generate(
+			self.sequence, 
+			self.time
+			)	
+		self.base_image = self.draw_base()
+		self.symmetry_image = self.draw_symmetry()
 
-	def generate(self, sequence, length):
-		""" Generate a Thue-Morse sequence with user specified base and time 
+	def generate(self, sequence, duration):
+		"""
+			Generate a Thue-Morse sequence with user specified base and time 
 			values. Utilises modulo to allow base numbers other than 2.
 		"""
-		bus = []
-		for loop in range(length):
+		for loop in range(duration):
+			bus = []
 			for e in sequence:
 				for i in range(self.base):
 					bus.append((i+e) % self.base)
 			sequence = []
-			sequence.extend(bus)
-			bus = []
-		return sequence			
+			sequence.extend(bus)			
+		return sequence
 
-	def sum_set(self, key, bus=[]):
-		""" Iterates over the generated Thue-Morse and linear number sequences,
-			creating balanced sets of integers that are then power summed - 
-			demonstrating the balance and beauty of this particular sequence.
+	def draw_base(self):
 		"""
-		bus = []
-		for k,e in zip(self.sequence, self.number_set):
-			if k == key:
-				bus.append(e)
-		print(bus)
-		print('(n + n...) = ' + str(sum(bus)))
-		for power in range(self.time):
-			sum_set = 0
-			for i in bus:
-				sum_set += pow(i, power)
-			print('(n^' + str(power) + ' n^' + str(power) + '...) = ' + str(sum_set))
+		"""
+		d = len(self.sequence)
+		image = Image.new("RGB",(d, d))
+		draw = ImageDraw.Draw(image)
+		for y in range(d):
+			for x in range(d):
+				hue = 180
+				saturation = 100
+				lightness = modulo(
+					(self.sequence_square[(y+x)]*(100/self.base)), 
+					100
+					)
+				draw.point((x,y), f'hsl({hue},{saturation}%,{lightness}%)')		
+		return image
 
+	def draw_symmetry(self):
+		"""
+		"""
+		d = len(self.sequence)
+		image = Image.new("RGB", (d * 2, d * 2))
+		draw = ImageDraw.Draw(image)
+		image.paste(self.base_image, (0, 0))
+		image.paste(ImageOps.mirror(self.base_image), (d, 0))
+		image.paste(ImageOps.flip(self.base_image), (0, d))
+		image.paste(ImageOps.mirror(ImageOps.flip(self.base_image)), (d, d))
+		image.save(f'{self.name}.png', "PNG")
+		return image
 
-
-		
+	def groups(self):
+		"""
+			Iterates over the generated Thue-Morse sequence, creating balanced 
+			groups of integers - demonstrating the balance and beauty of this
+			particular sequence.
+		"""
+		groups = {key: None for key in [i for i in range(self.base)]}
+		for k,e in zip(self.sequence, [i for i in range(1, len(self.sequence))]):
+			groups[k].append(e)
+		return groups
+			
+ops = {
+	"+": operator.add,
+	"-": operator.sub
+	}
 
 def divide(a, b):
 	try:
@@ -54,39 +93,15 @@ def divide(a, b):
 	except ZeroDivisionError:
 		return 0
 
+def modulo(a, b):
+	try:
+		return a % b
+	except ZeroDivisionError:
+		return 0
 
-
-# please ignore the below ugliness, it's just scratchpad stuff atm
-
-
-
-
-
-	
-
-x = Sequence(5, 3)
-#print(x.sequence)
-for key in range(x.base):
-	x.sum_set(key)
-print(x.sequence)
-print(x.sequence_square)
-dx = 0
-dy = 0
-dimension = len(x.sequence)
-print(dimension)
-im = Image.new("RGB",(dimension, dimension))
-draw = ImageDraw.Draw(im)
-for e in x.sequence_square:
-	draw.point((dx,dy), 'hsl({},100%,50%)'.format(divide(360,e)))
-	dx += 1
-	if dx == (dimension):
-		dy += 1
-		dx = 0
-	#dx += 1
-
-
-
-im.save("test.png", "PNG")
+n = int(input('base?: '))
+t = int(input('time?: '))
+sequences = {f'{n}{t}{i}': Sequence(n, t, i) for i in range(n)}
 
 
 
