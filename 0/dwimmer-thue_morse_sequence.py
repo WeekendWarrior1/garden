c410827245846e35323eeff0f1b43bc03120524e
dwimmer-tm <dwimmer.tm@pm.me>
2021-10-12 00:40:39 +1100

    moved program execution to main(), and class execution to run(). reduced clutter: removed divide and modulo functions.

--- 0/dwimmer-thue_morse_sequence.py
+++ 0/dwimmer-thue_morse_sequence.py
@@ -1,6 +1,16 @@
+# A simple program to explore (some of) the interesting properties of the
+# Thue-Morse sequence.
+#
 # dwimmer 2021
-import sys, operator
+
+import operator
 from PIL import Image, ImageDraw, ImageOps
+
+
+ops = {
+	"+": operator.add,
+	"-": operator.sub
+	}
 
 
 class Sequence:
@@ -8,24 +18,15 @@ class Sequence:
 		A simple program to explore (some of) the interesting properties of the
 		Thue-Morse sequence. 
 	'''
-	def __init__(self, base, time, index, operator="+"):
+	def __init__(self, base, time, index, op="+"):
 		""" 
 		
 		"""
 		self.base = base
 		self.time = time 
 		self.index = index
+		self.op = op
 		self.name = f'n{self.base}t{self.time}i{self.index}'
-		self.sequence = self.generate(
-			[(ops[operator](i, index)) % self.base for i in range(self.base)], 
-			(self.time - 1)
-			)
-		self.sequence_square = self.generate(
-			self.sequence, 
-			self.time
-			)	
-		self.base_image = self.draw_base()
-		self.symmetry_image = self.draw_symmetry()
 
 	def generate(self, sequence, duration):
 		"""
@@ -41,34 +42,30 @@ class Sequence:
 			sequence.extend(bus)			
 		return sequence
 
-	def draw_base(self):
+	def draw_base(self, sequence, sequence_square):
 		"""
 		"""
-		d = len(self.sequence)
+		d = len(sequence)
 		image = Image.new("RGB",(d, d))
 		draw = ImageDraw.Draw(image)
 		for y in range(d):
 			for x in range(d):
-				hue = 180
+				hue = 268
 				saturation = 100
-				lightness = modulo(
-					(self.sequence_square[(y+x)]*(100/self.base)), 
-					100
-					)
+				lightness = (sequence_square[(y+x)]*(100/self.base)) % 100
 				draw.point((x,y), f'hsl({hue},{saturation}%,{lightness}%)')		
 		return image
 
-	def draw_symmetry(self):
+	def draw_symmetry(self, base_image):
 		"""
 		"""
-		d = len(self.sequence)
+		d = base_image.width
 		image = Image.new("RGB", (d * 2, d * 2))
 		draw = ImageDraw.Draw(image)
-		image.paste(self.base_image, (0, 0))
-		image.paste(ImageOps.mirror(self.base_image), (d, 0))
-		image.paste(ImageOps.flip(self.base_image), (0, d))
-		image.paste(ImageOps.mirror(ImageOps.flip(self.base_image)), (d, d))
-		image.save(f'{self.name}.png', "PNG")
+		image.paste(base_image, (0, 0))
+		image.paste(ImageOps.mirror(base_image), (d, 0))
+		image.paste(ImageOps.flip(base_image), (0, d))
+		image.paste(ImageOps.mirror(ImageOps.flip(base_image)), (d, d))
 		return image
 
 	def groups(self):
@@ -78,30 +75,40 @@ class Sequence:
 			particular sequence.
 		"""
 		groups = {key: None for key in [i for i in range(self.base)]}
-		for k,e in zip(self.sequence, [i for i in range(1, len(self.sequence))]):
+		for k,e in zip(
+			self.sequence, 
+			[i for i in range(1, len(self.sequence))]
+			):
 			groups[k].append(e)
 		return groups
+
+	def run(self):
+		"""
+		"""
+		sequence = self.generate(
+			[ops[self.op](i, self.index) % self.base for i in range(self.base)], 
+			(self.time - 1)
+			)
+		sequence_square = self.generate(
+			sequence, 
+			self.time
+			)	
+		base_image = self.draw_base(sequence, sequence_square)
+		symmetry_image = self.draw_symmetry(base_image)
+		symmetry_image.save(f'{self.name}.png', "PNG")
 			
-ops = {
-	"+": operator.add,
-	"-": operator.sub
-	}
 
-def divide(a, b):
-	try:
-		return a / b
-	except ZeroDivisionError:
-		return 0
+def main():	
+	"""
+	"""
+	n = int(input('base?: '))
+	t = int(input('time?: '))
+	sequences = {f'{n}{t}{i}': Sequence(n, t, i) for i in range(n)}
+	for e in sequences:
+		sequences[e].run()
 
-def modulo(a, b):
-	try:
-		return a % b
-	except ZeroDivisionError:
-		return 0
-
-n = int(input('base?: '))
-t = int(input('time?: '))
-sequences = {f'{n}{t}{i}': Sequence(n, t, i) for i in range(n)}
+if __name__ == "__main__":
+	main()
 
 
 
