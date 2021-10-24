0ca7d63a98bc834914e193395dda19af3be7ec49
dwimmer-tm <dwimmer.tm@pm.me>
2021-10-24 13:48:24 +1100

    tidied sleep, fixed redraw bug

--- 0/curses/dwimmer-PRiSM.py
+++ 0/curses/dwimmer-PRiSM.py
@@ -1,12 +1,13 @@
 # a fun little program that renders a simple 4D prism keyed off a dynamic
 # Thue-Morse sequence keyed off the current (resizable!) terminal dimensions.
 # pardon the somewhat ugly (if traditional) characters - it's a WIP ;D
+#
+# considerably less jank - special thanks to WeekendWarrior1 <3
 # 
-# dwimmer 2021 <3
+# dwimmer 2021
 
-import curses
+import curses, time
 from itertools import product
-import time
 
 def main():
 	'''
@@ -14,18 +15,6 @@ def main():
 	'''
 	curses.wrapper(curses_main)
 
-
-def curses_sleep(fps: int, time_started_update: int):
-	'''
-		Strive for target fps regardless of processing time
-		Initial calculations are all done in ns
-	'''
-	now = time.time_ns()
-	time_to_sleep_ns = (1000000000 / fps) - (now - time_started_update)
-	time_to_sleep_ms = int(time_to_sleep_ns // 1000000)
-	# could be negative number if the render code took greater than 17ms
-	if time_to_sleep_ms > 0:
-		curses.napms(time_to_sleep_ms)
 
 def curses_main(w):
 	'''
@@ -47,27 +36,6 @@ def curses_main(w):
 			n, fy, fx = resize(w)
 
 		prism(w, n, fy, fx)
-		
-
-def modulo(a, b):
-	'''
-		Returns modulo arithmetic - allowing for result of zero.
-		:arg:		a 			4D prism coordinate
-		:arg:		b 			base number relative to terminal dimensions
-	'''
-	try:
-		return a % b
-	except ZeroDivisionError:
-		return 0	    
-
-
-def centre(a, b):
-	'''
-		Returns 0,0 position of prism relative to current screen centre.
-		:arg:		a 			dimension of frame/terminal
-		:arg:		b 			dimension of prism array 
-	'''
-	return (round(a//2) - round(b//2))
 
 
 def resize(w):
@@ -81,7 +49,37 @@ def resize(w):
 	return n, fy, fx
 
 
-def render(w, fy, fx, prism, r, time_started_update):
+def prism(w, n, fy, fx):	
+	'''
+		Generate slices of a 4D Thue-Morse prism and pass to render().
+		:var:		prism		2D array of colour values
+		:var:		r 			dimension of prism array
+		:var:		timestamp	mark, in nanoseconds, time before processing
+	'''
+	r = (n*2)-1
+
+	for t in range(n):
+		timestamp = time.time_ns()
+		prism = [[0 for __ in range(r)] for __ in range(r)]
+		for z, y, x in product(range(n), range(n), range(n)):
+			if prism[y+z][x+z] >> 5 == 0:
+				prism[y+z][x+z] = modulo((t+z+y+x), n) + 32
+		render(w, fy, fx, prism, r, timestamp)
+
+
+def modulo(a, b):
+	'''
+		Returns modulo arithmetic - allowing for result of zero.
+		:arg:		a 			4D prism coordinate
+		:arg:		b 			base number relative to terminal dimensions
+	'''
+	try:
+		return a % b
+	except ZeroDivisionError:
+		return 0	
+
+
+def render(w, fy, fx, prism, r, timestamp):
 	'''
 		Erases screen and renders the provided prism slice at centre screen.
 	'''
@@ -97,27 +95,32 @@ def render(w, fy, fx, prism, r, time_started_update):
 						curses.color_pair(232+(prism[y][x]-32))
 						)
 	except:
-		pass
+		fy, fx = w.getmaxyx()
+		render(w, fy, fx, prism, r, timestamp)
+
 	w.refresh()
-	curses_sleep(60, time_started_update)
+	curses_sleep(timestamp)
 
-	
-def prism(w, n, fy, fx):	
-	'''
-		Generate slices of a 4D Thue-Morse prism and pass to render().
-		:var:		prism		2D array of colour values
-		:var:		r 			dimension of prism array
-	'''
-	r = (n*2)-1
 
-	for t in range(n):
-		time_started_update = time.time_ns()
-		prism = [[0 for __ in range(r)] for __ in range(r)]
-		for z, y, x in product(range(n), range(n), range(n)):
-			if prism[y+z][x+z] >> 5 == 0:
-				prism[y+z][x+z] = modulo((t+z+y+x), n) + 32
-		render(w, fy, fx, prism, r, time_started_update)
-	
+def centre(a, b):
+	'''
+		Returns 0,0 position of prism relative to current screen centre.
+		:arg:		a 			dimension of frame/terminal
+		:arg:		b 			dimension of prism array 
+	'''
+	return (round(a//2) - round(b//2))
+    
+
+def curses_sleep(timestamp):
+	'''
+		Strive for 60 fps regardless of processing time. 
+		Initial calculations are all done in nanoseconds.
+		:var:		nap 		determines elapsed time since timestamp in ms
+	'''
+	nap = int(((1000000000 / 60) - (time.time_ns() - timestamp)) // 1000000)
+	if nap > 0: curses.napms(nap)
+
+
 
 main()
 		
